da567ab661fb01b901d829169b2a4aa89262efe0
Chance-ctrl <65079828+Chance-ctrl@users.noreply.github.com>
2025-08-19 08:31:42 -0500

    Update PoshMarkAutoReshare.py
    fix scrolling, added check for closet beta pop-up. changed xpath for available items

--- PoshMarkAutoReshare.py
+++ PoshMarkAutoReshare.py
@@ -28,50 +28,56 @@ def PoshLogin():
 
  
 def load_all_listings():
-    scroll_pause_time = 15
-    # After login this Click Events Open your Closet and Select only Available Items.
+    scroll_pause_time = 10
+    # These elements go to your closet
     driver.find_element(By.XPATH, '//*[@id="app"]/header/nav[1]/div/ul/li[5]/div').click()
     driver.find_element(By.XPATH,'//*[@id="app"]/header/nav[1]/div/ul/li[5]/div/div[2]/div/ul/li[1]').click()
-    driver.find_element(By.XPATH, '//*[@id="content"]/div/div/div[2]/div/div[3]/nav/div/div[9]/div/div[2]/ul/li[2]/div/label/div').click()
+    
+    try:
+        time.sleep(.5)
+        driver.find_element(By.XPATH,'//*[@id="content"]/div[2]/div/div[2]/div[1]/button').click()  
+        print("close pop-up")
+    except:
+        print(" didn't find pop-up of closet beta thing")
+
+    # this element is to find the Available Items button and click it
+    driver.find_element(By.XPATH, '//*[@id="content"]/div[1]/div/div[2]/div/div[2]/nav/div/div[9]/div/div[2]/ul/li[2]/div/label/div').click()
+    # Get scroll height
     last_height = driver.execute_script("return document.body.scrollHeight")
 
     while True:
+        # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        time.sleep(scroll_pause_time)
-
+        # Wait for new content to load, TODO make this dynamic based on page response
+        time.sleep(1.5)
+        # Calculate new scroll height and compare with last scroll height
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
             break
         last_height = new_height
 
-
 def share_to_followers(share_btn):
-
     actions.move_to_element(share_btn).perform() 
     try:
+        time.sleep(.7)
         share_btn.click()
         to_my_followers = driver.find_element(By.CLASS_NAME,"share-wrapper-container")
-        to_my_followers.click()
-    except:
-            # TODO - Captcha Bypass : Can run in debug mode and put a break point here to catch.
-            print("captcha block") 
-
+        to_my_followers.click()     
+    except Exception as e:
+        print(f"captcha block {e}") 
     time.sleep(.2)
 
 def share_listings(share_buttons):
     print("There are: ", len(share_buttons), " listings")
-
-    i = 0
+    i = 0 
     for share_btn in share_buttons:
         share_to_followers(share_btn)
         i += 1
         print("Shared Listing: #", i)
   
-
-
 if __name__ == '__main__':
     PoshLogin()
     load_all_listings()
     share_buttons = driver.find_elements(By.XPATH, '//div[@data-et-name="share"]')
     share_listings(share_buttons)
+    driver.close()
